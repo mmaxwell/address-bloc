8e6e6c6745dd177fcc64d19854e8be8dbbd0fc6a
Matthew Maxwell <matthew.maxwell@bloc.io>
2016-08-25 07:36:48 +0000

    Update to using ARGV.shift

--- greeting.rb
+++ greeting.rb
@@ -1,14 +1,24 @@
 # The instructions didn't say to wrap this in a function
 # so I'm not doing that
 
-# Is there a better way here?
-greeting = ""
+# Prior to learning about ARGV.shift
 
+# Is there a better way here?
+# greeting = ""
 # We assume the first argument is always the greeting
-ARGV.each_with_index do | arg, index |
-    if index == 0
-        greeting = arg
-    else
-        puts "#{greeting} #{arg}"
-    end
+# ARGV.each_with_index do | arg, index |
+#     if index == 0
+#         greeting = arg
+#     else
+#         puts "#{greeting} #{arg}"
+#     end
+# end
+
+# I Couldn't seem to find where it talked about shift.
+# I looked in the Array section, but maybe I missed it.
+# The gist of this is that I can use array methods on ARGV, correct?
+greeting = ARGV.shift
+
+ARGV.each do | name |
+    puts "#{greeting} #{name}"
 end
