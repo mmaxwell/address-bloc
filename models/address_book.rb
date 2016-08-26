c705c392dc904da3e2a5cc75c09e3c4a19fb64e8
Matthew Maxwell <matthew.maxwell@bloc.io>
2016-08-26 02:19:43 +0000

    Add remove_entry and tests

--- models/address_book.rb
+++ models/address_book.rb
@@ -20,4 +20,12 @@ class AddressBook
         
         entries.insert(index, Entry.new(name, phone_number, email))
     end
+    
+    def remove_entry(name, phone_number, email) 
+        # Find the index of the entry with the name, phone number, and email provided
+        index = entries.index { | entry | entry.name == name && entry.phone_number && entry.email == email }
+        # If the entry is not found, index will be nil and delete_at will error, so
+        # only delete if the index is not nil.
+        entries.delete_at index unless index.nil?
+    end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -2,18 +2,17 @@ require_relative "../models/address_book"
 
 RSpec.describe AddressBook do
     describe "attributes" do
+        let (:book) { AddressBook.new }
+        
         it "responds to entries" do
-            book = AddressBook.new
             expect(book).to respond_to(:entries)
         end
         
         it "initializes entries as an array" do
-            book = AddressBook.new
             expect(book.entries).to be_an(Array)
         end
         
         it "initializes entries as empty" do
-            book = AddressBook.new
             expect(book.entries.size).to eq(0)
         end
     end
@@ -34,6 +33,29 @@ RSpec.describe AddressBook do
             expect(new_entry.name).to eq("Ada Lovelace")
             expect(new_entry.phone_number).to eq("010.012.1815")
             expect(new_entry.email).to eq("augusta.king@lovelace.com")
+        end
+    end
+    
+    describe "#remove_entry" do
+        it "removes only one entry" do
+            book = AddressBook.new
+            book.add_entry("Ada Lovelace", "010.012.1815", "augusta.king@lovelace.com")
+            book.add_entry("Peter Griffin", "123.867.5309", "p@griffin.com")
+            
+            book.remove_entry("Peter Griffin", "123.867.5309", "p@griffin.com")
+            expect(book.entries.size).to eq(1)
+        end
+        
+        it "removes the correct entry" do
+            book = AddressBook.new
+            book.add_entry("Ada Lovelace", "010.012.1815", "augusta.king@lovelace.com")
+            book.add_entry("Peter Griffin", "123.867.5309", "p@griffin.com")
+            book.add_entry("Louis Griffin", "123.867.5310", "l@griffin.com")
+            
+            book.remove_entry("Peter Griffin", "123.867.5309", "p@griffin.com")
+            
+            expect(book.entries[0].name).to eq("Ada Lovelace")
+            expect(book.entries[1].name).to eq("Louis Griffin")
         end
     end
 end
